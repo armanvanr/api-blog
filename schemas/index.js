1f0709cb1b2088bec79821a637bd4a299597577a
Venska Arman Nur Rosyidin <armanvenska@gmail.com>
2022-11-24 16:54:06 +0700

    connect to remote mongoDB

--- schemas/index.js
+++ schemas/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = () => {
   mongoose
-    .connect("mongodb://localhost:27017/w6_blog")
+    .connect("mongodb+srv://armanvenska:ezpeasy@cluster0.fiyusps.mongodb.net/?retryWrites=true&w=majority")
     .catch(err => console.log(err));
 };
 
